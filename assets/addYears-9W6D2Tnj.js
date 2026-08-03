b9d55085bf6b5d2c057fc383d137b37574dd8659
Laura Nowak <laura.nowak.kontakt@gmail.com>
2026-08-03 17:55:26 +0200

    Updates

--- assets/addYears-9W6D2Tnj.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./subMonths-sxOvZ9nC.js";function d(a,r){return o(a,r*12)}export{d as a};
---
+++ assets/subWeeks-BHZmuNWM.js
@@ -0,0 +1,1 @@
+import{a as s}from"./endOfMonth-DeEwXcQL.js";function r(e,a){return s(e,-1)}export{r as s};
