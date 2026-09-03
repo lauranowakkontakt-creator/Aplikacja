8b666abd2b1849769937472be53393a6f10e111d
Laura Nowak <laura@Mac-mini-Laura.local>
2026-09-03 10:39:32 +0200

    Updates

--- assets/getDate-B53Jf2WW.js
+++
@@ -1,1 +0,0 @@
-import{b3 as a}from"./index-D6c5yefh.js";function r(t){return a(t).getDate()}export{r as g};
---
+++ assets/getDaysInMonth-BY6yMoAj.js
@@ -0,0 +1,1 @@
+import{b4 as a,ad as r}from"./index-Ccxc0rpV.js";function u(o){const e=a(o),n=e.getFullYear(),s=e.getMonth(),t=r(o,0);return t.setFullYear(n,s+1,0),t.setHours(0,0,0,0),t.getDate()}export{u as g};
