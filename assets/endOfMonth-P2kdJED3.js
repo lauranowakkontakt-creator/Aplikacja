fec9e2c3bf7eefa9ac43962d2eb68ecd4c5b0acb
Laura Nowak <laura@Mac-mini-Laura.Dlink>
2026-09-04 14:49:32 +0200

    Updates

--- assets/endOfMonth-P2kdJED3.js
+++
@@ -1,1 +0,0 @@
-import{b4 as n}from"./index-Ccxc0rpV.js";function s(e){const t=n(e),o=t.getMonth();return t.setFullYear(t.getFullYear(),o+1,0),t.setHours(23,59,59,999),t}export{s as e};
---
+++ assets/startOfMonth-DOHLEdKN.js
@@ -0,0 +1,1 @@
+import{b4 as s}from"./index-N9hSwAGx.js";function r(o){const t=s(o);return t.setDate(1),t.setHours(0,0,0,0),t}export{r as s};
